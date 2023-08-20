b939ae5b1df2a623985c2e3422f8368945fead13
Angel-Jesua <142702911+Angel-Jesua@users.noreply.github.com>
2023-08-20 13:43:08 -0600

    Add files via upload

--- Pregunta de Herencia/Herencia.cs
+++ Pregunta de Herencia/Herencia.cs
@@ -11,17 +11,33 @@ namespace Herencia
         static void Main(string[] args)
         {
             medico med = new medico();
-
             string nombre;
             string apellidos;
+            int anoingreso;
+            int antiguedadanos;
+            int salario;
+            int horas;
             Console.WriteLine("Ingrese su nombre");
             nombre = Console.ReadLine();
             Console.WriteLine("Ingrese su apellido");
             apellidos = Console.ReadLine();
-            Console.WriteLine("Ingrese el año en el que entro");
-            int anoingreso = Convert.ToInt32(Console.ReadLine());
-            int antiguedadanos = med.CalcularAntiguedad(new DateTime(anoingreso, 1, 1));
-            Console.WriteLine("Su antiguedad es: "+antiguedadanos+ "años");
+            do
+            {
+                Console.WriteLine("Ingrese el año en el que entro");
+                anoingreso = Convert.ToInt32(Console.ReadLine());
+                antiguedadanos = med.CalcularAntiguedad(new DateTime(anoingreso, 1, 1));
+            } while (anoingreso < 1970 || anoingreso > 2023);
+            do {
+                Console.WriteLine("Ingrese en dolares cuanto gana la hora");
+                salario = Convert.ToInt32(Console.ReadLine());
+            } while (salario<0||salario>35);
+            do
+            {
+                Console.WriteLine("Ingrese cuantas horas trabaja");
+                horas = Convert.ToInt32(Console.ReadLine());
+            } while (horas<0||horas>10);
+            int CalculoDeSalario = med.CalculoSalario(antiguedadanos, salario);
+            Console.WriteLine("Usted ha trabajado {0} años y gana {1} dolares la hora", antiguedadanos, CalculoDeSalario);
         }
     }
 }
