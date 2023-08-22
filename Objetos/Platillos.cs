45e1ec7b592af3f607df2e619259fc906691d1ab
Angel-Jesua <142702911+Angel-Jesua@users.noreply.github.com>
2023-08-21 20:29:31 -0600

    Add files via upload

---
+++ Objetos/Platillos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    internal class Platillos
+    {
+        public string ingredientes { get; set; }
+        public string Nombre { get; set; }
+
+        public void MostrarIngredientes()
+        {
+            Console.WriteLine($"Los ingredientes del platillo {Nombre} es: {ingredientes}.");
+        }
+        public void Calentar()
+        {
+            Console.WriteLine($"Calentando el platillo {Nombre}.");
+        }
+    }
+
+}
